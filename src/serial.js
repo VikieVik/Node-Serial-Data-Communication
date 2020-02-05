7ac9df8abef940872b27eca9268771835663c6b7
Vikas Singh <vikas@Singhs-MacBook-Air.local>
2020-02-06 00:30:50 +0530

    added basic comment

--- src/serial.js
+++ src/serial.js
@@ -1,8 +1,15 @@
-//Serial com with arduino and node js
+/*
+This Node js app lets us communicate with serial devices spitting serial data
+Author: Trident Holder
+Date:05 Jan, 2020
+
+
+*/
+
 var SerialPort = require('serialport')
 var port = new SerialPort(
-	'/dev/tty.usbmodem14201',
-	{ baudRate: 9600 }, 
+	'/dev/tty.usbmodem14201', //Find where your device is connected and replace here
+	{ baudRate: 9600 }, // baudRate should match with hardware baudrate
 	function(err){
 		if(err){
 			return console.log('Error: ' , err.message);
@@ -11,5 +18,6 @@ var port = new SerialPort(
 );
 
 port.on('data',function(data) {
-	process.stdout.write(data.toString());
+	process.stdout.write(data.toString()); //process.stdout.write() function prints data 
+	                                        // unlike console.log() which adds /n after logging
 });
